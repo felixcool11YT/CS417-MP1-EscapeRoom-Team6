6eb46fa7674ec88b1ae95f688853f2fd08bc4d54
Daryl Okeke <dokeke2004@gmail.com>
2026-09-22 03:28:39 -0500

    Added interaction Mask

---
+++ Assets/EscapeRoom/Rooms/Daryl_BedroomDecon/Scripts/Puzzle.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    private XRGrabInteractable FilterParticulate;
+
+    [SerializeField]
+    private XRGrabInteractable FilterChemical;
+
+    [SerializeField]
+    private XRGrabInteractable FilterRadiation;
+
+}
